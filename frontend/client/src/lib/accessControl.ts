7fe32f9f667edc66c1488266daf4450672362180
FelipeOliveiraMADM <felipe.oliveiramadm@gmail.com>
2026-08-26 13:41:26 -0300

    Ajuste cargo Denis

--- frontend/client/src/lib/accessControl.ts
+++ frontend/client/src/lib/accessControl.ts
@@ -92,6 +92,7 @@ const GROUP_MAPPING: Record<string, Level> = {
   'analista de dados': LEVELS.ADMINISTRATIVO,
   'desenvolvedor make': LEVELS.ADMINISTRATIVO,
   'analista de discadora': LEVELS.ADMINISTRATIVO,
+  'Supervisor Sales Ops': LEVELS.ADMINISTRATIVO,
 
   // Super Admin
   'desenvolvedor': LEVELS.SUPER_ADMIN,
